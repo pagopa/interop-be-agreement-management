391f8a8a356c8347f5ac29028c11065cbda98d97
P3trur0 <ubaldopescatore@gmail.com>
2021-10-12 10:51:35 +0200

    added versioning mechanism

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,12 +1,12 @@
 object Versions {
-  lazy val akkaVersion                 = "2.6.15"
-  lazy val akkaHttpVersion             = "10.2.4"
-  lazy val awsSdkVersion               = "2.4.17"
+  lazy val akkaVersion                 = "2.6.16"
+  lazy val akkaHttpVersion             = "10.2.6"
+  lazy val awsSdkVersion               = "2.17.56"
   lazy val catsVersion                 = "2.6.0"
   lazy val projectionVersion           = "1.2.1"
   lazy val cassandraPersistenceVersion = "1.0.5"
   lazy val s3Persistence               = "1.0.27"
-  lazy val httpJson4sVersion           = "1.37.0"
+  lazy val httpJson4sVersion           = "1.38.2"
   lazy val akkaManagementVersion       = "1.0.10"
   lazy val json4sVersion               = "4.0.3"
   lazy val kamonVersion                = "2.1.17"
@@ -16,4 +16,5 @@ object Versions {
   lazy val logbackVersion              = "1.2.3"
   lazy val scalatestVersion            = "3.2.8"
   lazy val scalaMockVersion            = "5.1.0"
+  lazy val mustacheVersion             = "0.9.0"
 }

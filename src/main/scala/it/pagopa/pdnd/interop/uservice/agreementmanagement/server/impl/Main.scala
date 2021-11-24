003c9b9e267b354be5928d4fdc9375e9e389bd59
Stefano Perazzolo <stefano.perazzolo@pagopa.it>
2021-11-24 17:18:45 +0100

    PIN 807: add client enum codec

--- src/main/scala/it/pagopa/pdnd/interop/uservice/agreementmanagement/server/impl/Main.scala
+++ src/main/scala/it/pagopa/pdnd/interop/uservice/agreementmanagement/server/impl/Main.scala
@@ -15,9 +15,16 @@ import akka.persistence.typed.PersistenceId
 import akka.projection.ProjectionBehavior
 import akka.{actor => classic}
 import it.pagopa.pdnd.interop.uservice.agreementmanagement.api.AgreementApi
-import it.pagopa.pdnd.interop.uservice.agreementmanagement.api.impl.{AgreementApiMarshallerImpl, AgreementApiServiceImpl}
+import it.pagopa.pdnd.interop.uservice.agreementmanagement.api.impl.{
+  AgreementApiMarshallerImpl,
+  AgreementApiServiceImpl
+}
 import it.pagopa.pdnd.interop.uservice.agreementmanagement.common.system.{ApplicationConfiguration, Authenticator}
-import it.pagopa.pdnd.interop.uservice.agreementmanagement.model.persistence.{AgreementPersistentBehavior, AgreementPersistentProjection, Command}
+import it.pagopa.pdnd.interop.uservice.agreementmanagement.model.persistence.{
+  AgreementPersistentBehavior,
+  AgreementPersistentProjection,
+  Command
+}
 import it.pagopa.pdnd.interop.uservice.agreementmanagement.server.Controller
 import it.pagopa.pdnd.interop.uservice.agreementmanagement.service.UUIDSupplier
 import it.pagopa.pdnd.interop.uservice.agreementmanagement.service.impl.UUIDSupplierImpl

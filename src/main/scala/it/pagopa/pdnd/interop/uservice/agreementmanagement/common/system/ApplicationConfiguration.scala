b6c53413b9133f0fd9ce61e1fc3d7e21d731fa47
Stefano Perazzolo <stefano.perazzolo@pagopa.it>
2022-02-08 12:44:21 +0100

    update commons lib

--- src/main/scala/it/pagopa/pdnd/interop/uservice/agreementmanagement/common/system/ApplicationConfiguration.scala
+++ src/main/scala/it/pagopa/pdnd/interop/uservice/agreementmanagement/common/system/ApplicationConfiguration.scala
@@ -2,11 +2,13 @@ package it.pagopa.pdnd.interop.uservice.agreementmanagement.common.system
 
 import com.typesafe.config.{Config, ConfigFactory}
 
+import scala.jdk.CollectionConverters.ListHasAsScala
+
 object ApplicationConfiguration {
   lazy val config: Config = ConfigFactory.load()
 
-  def serverPort: Int = {
-    config.getInt("uservice-agreement-management.port")
-  }
+  def serverPort: Int = config.getInt("uservice-agreement-management.port")
+
+  def jwtAudience: Set[String] = config.getStringList("uservice-party-management.jwt.audience").asScala.toSet
 
 }
--- src/main/scala/it/pagopa/pdnd/interop/uservice/agreementmanagement/server/impl/Main.scala
+++ src/main/scala/it/pagopa/pdnd/interop/uservice/agreementmanagement/server/impl/Main.scala
@@ -14,8 +14,10 @@ import akka.management.scaladsl.AkkaManagement
 import akka.persistence.typed.PersistenceId
 import akka.projection.ProjectionBehavior
 import akka.{actor => classic}
+import com.nimbusds.jose.proc.SecurityContext
+import com.nimbusds.jwt.proc.DefaultJWTClaimsVerifier
 import it.pagopa.pdnd.interop.commons.jwt.service.JWTReader
-import it.pagopa.pdnd.interop.commons.jwt.service.impl.DefaultJWTReader
+import it.pagopa.pdnd.interop.commons.jwt.service.impl.{DefaultJWTReader, getClaimsVerifier}
 import it.pagopa.pdnd.interop.commons.jwt.{JWTConfiguration, PublicKeysHolder}
 import it.pagopa.pdnd.interop.commons.utils.OpenapiUtils
 import it.pagopa.pdnd.interop.commons.utils.errors.GenericComponentErrors.ValidationRequestError
@@ -47,6 +49,8 @@ object Main extends App {
     keyset <- JWTConfiguration.jwtReader.loadKeyset()
     jwtValidator = new DefaultJWTReader with PublicKeysHolder {
       var publicKeyset = keyset
+      override protected val claimsVerifier: DefaultJWTClaimsVerifier[SecurityContext] =
+        getClaimsVerifier(audience = ApplicationConfiguration.jwtAudience)
     }
   } yield jwtValidator
 

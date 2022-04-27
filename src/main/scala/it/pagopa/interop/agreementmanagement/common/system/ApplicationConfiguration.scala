c9e25ee09e387c4a96dd1e02aa88083d5f38d5d3
galales <25105748+galales@users.noreply.github.com>
2022-04-27 14:54:08 +0200

    pin-1382: Verify the audience configuration is set

--- src/main/scala/it/pagopa/interop/agreementmanagement/common/system/ApplicationConfiguration.scala
+++ src/main/scala/it/pagopa/interop/agreementmanagement/common/system/ApplicationConfiguration.scala
@@ -3,13 +3,15 @@ package it.pagopa.interop.agreementmanagement.common.system
 import com.typesafe.config.{Config, ConfigFactory}
 
 object ApplicationConfiguration {
-  lazy val config: Config = ConfigFactory.load()
+  val config: Config = ConfigFactory.load()
 
-  lazy val serverPort: Int = config.getInt("agreement-management.port")
+  val serverPort: Int = config.getInt("agreement-management.port")
 
-  lazy val jwtAudience: Set[String] = config.getString("agreement-management.jwt.audience").split(",").toSet
+  val jwtAudience: Set[String] = config.getString("agreement-management.jwt.audience").split(",").toSet
 
-  lazy val numberOfProjectionTags: Int = config.getInt("akka.cluster.sharding.number-of-shards")
-  def projectionTag(index: Int)        = s"interop-be-agreement-management-persistence|$index"
-  lazy val projectionsEnabled: Boolean = config.getBoolean("akka.projection.enabled")
+  val numberOfProjectionTags: Int = config.getInt("akka.cluster.sharding.number-of-shards")
+  def projectionTag(index: Int)   = s"interop-be-agreement-management-persistence|$index"
+  val projectionsEnabled: Boolean = config.getBoolean("akka.projection.enabled")
+
+  require(jwtAudience.nonEmpty, "Cannot have an empty audience")
 }

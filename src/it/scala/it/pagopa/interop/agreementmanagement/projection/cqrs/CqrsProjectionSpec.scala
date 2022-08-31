675521b63d7453da09356cfd625d48253d69372d
galales <25105748+galales@users.noreply.github.com>
2022-08-31 11:05:07 +0200

    pin-1710: Update integration tests

--- src/it/scala/it/pagopa/interop/agreementmanagement/projection/cqrs/CqrsProjectionSpec.scala
+++ src/it/scala/it/pagopa/interop/agreementmanagement/projection/cqrs/CqrsProjectionSpec.scala
@@ -3,9 +3,9 @@ package it.pagopa.interop.agreementmanagement.projection.cqrs
 import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
 import it.pagopa.interop.agreementmanagement.ItSpecData._
 import it.pagopa.interop.agreementmanagement.model.ChangedBy.PRODUCER
+import it.pagopa.interop.agreementmanagement.model.StateChangeDetails
 import it.pagopa.interop.agreementmanagement.model.agreement.{Active, Pending, PersistentAgreement}
 import it.pagopa.interop.agreementmanagement.model.persistence.JsonFormats._
-import it.pagopa.interop.agreementmanagement.model.{StateChangeDetails, VerifiedAttributeSeed}
 import it.pagopa.interop.agreementmanagement.{ItSpecConfiguration, ItSpecHelper}
 
 class CqrsProjectionSpec extends ScalaTestWithActorTestKit(ItSpecConfiguration.config) with ItSpecHelper {
@@ -48,17 +48,26 @@ class CqrsProjectionSpec extends ScalaTestWithActorTestKit(ItSpecConfiguration.c
       compareAgreements(expected, persisted)
     }
 
-    "succeed for event VerifiedAttributeUpdated" in {
+    "succeed for event AgreementConsumerDocumentAdded" in {
       val agreement = createAgreement(persistentAgreement.copy(state = Active))
 
-      val existingAttribute = agreement.verifiedAttributes.head
-      val seed              = VerifiedAttributeSeed(
-        id = existingAttribute.id,
-        verified = existingAttribute.verified.map(!_),
-        validityTimespan = existingAttribute.validityTimespan.map(_ + 10)
-      )
+      val document = persistentConsumerDocument
 
-      val expected  = updateVerifiedAttribute(agreement.id, seed)
+      val createdDoc = addConsumerDocument(agreement.id, document)
+      val expected   = agreement.copy(consumerDocuments = agreement.consumerDocuments :+ createdDoc)
+      val persisted  = findOne[PersistentAgreement](expected.id.toString).futureValue
+
+      compareAgreements(expected, persisted)
+    }
+
+    "succeed for event AgreementConsumerDocumentRemoved" in {
+      val document          = persistentConsumerDocument
+      val existingAgreement =
+        persistentAgreement.copy(state = Active, consumerDocuments = Seq(persistentConsumerDocument, document))
+      val agreement         = createAgreement(existingAgreement)
+
+      removeConsumerDocument(agreement.id, document.id)
+      val expected  = agreement.copy(consumerDocuments = agreement.consumerDocuments.filter(_.id != document.id))
       val persisted = findOne[PersistentAgreement](expected.id.toString).futureValue
 
       compareAgreements(expected, persisted)
